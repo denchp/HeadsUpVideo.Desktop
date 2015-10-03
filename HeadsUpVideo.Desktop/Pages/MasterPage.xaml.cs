71ca5e4f4f30d6d50c9d1b494793044ac9700a92
Patrick Dench <denchp@gmail.com>
2015-10-02 21:28:21 -0500

    Missing code behinds

---
+++ HeadsUpVideo.Desktop/Pages/MasterPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Windows.UI.Xaml.Controls;
+using HeadsUpVideo.Desktop.ViewModels;
+
+namespace HeadsUpVideo.Desktop.Pages
+{
+    public sealed partial class MasterPage : Page
+    {
+        public MasterPageViewModel viewModel = new MasterPageViewModel();
+
+        public MasterPage()
+        {
+            this.DataContext = viewModel;
+            this.InitializeComponent();
+            
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            this.Loaded += MasterPage_Loaded;
+            viewModel.NavigateTo += ViewModel_NavigateTo;
+        }
+
+        private void ViewModel_NavigateTo(object sender, System.Type e)
+        {
+            frameBody.Navigate(e);
+        }
+
+        private void MasterPage_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            viewModel.Initialize();
+        }
+    }
+}
---
+++ HeadsUpVideo.Desktop/Pages/WelcomePage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using HeadsUpVideo.Desktop.ViewModels;
+using HeadsUpVideo.Desktop.CustomControls;
+using System.Linq;
+
+namespace HeadsUpVideo.Desktop.Pages
+{
+    public sealed partial class WelcomePage : Page
+    {
+        internal WelcomePageViewModel viewModel = new WelcomePageViewModel();
+
+        public WelcomePage()
+        {
+            this.DataContext = viewModel;
+            this.InitializeComponent();
+            
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+           
+        }
+    }
+}
